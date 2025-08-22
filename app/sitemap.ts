a24c3bb30f1112f5e36ce15d8373dbaf25926c22
JaStaGa <jaylinstarlinganaway@gmail.com>
2025-08-22 14:07:30 -0400

    added routes to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,8 +2,14 @@ import { MetadataRoute } from "next";
 import { site } from "@/lib/seo";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+    const now = new Date();
     return [
-        { url: site.url, lastModified: new Date() },
-        { url: `${site.url}/menu`, lastModified: new Date() },
+        { url: site.url, lastModified: now },
+        { url: `${site.url}/menu`, lastModified: now },
+        { url: `${site.url}/about`, lastModified: now },
+        { url: `${site.url}/reserve`, lastModified: now },
+        { url: `${site.url}/contact`, lastModified: now },
+        { url: `${site.url}/gallery`, lastModified: now },
+        { url: `${site.url}/faq`, lastModified: now },
     ];
 }
